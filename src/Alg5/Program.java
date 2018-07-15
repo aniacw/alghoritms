61701991ca23de3fd0344fba7205eb998a6624b9
aniacw <cwiek.ania@gmail.com>
2018-07-15 23:18:13 +0200

    4alg

--- src/Alg5/Program.java
+++ src/Alg5/Program.java
@@ -2,34 +2,31 @@ package Alg5;
 
 public class Program {
 
-    public static int fib(int n){
+    public static int fib(int n) {
         System.out.println("fib(" + n + ")");
         if (n < 2) {
             System.out.println("return 1");
             return 1;
-        }
-        else {
-            System.out.println("return fib(" + (n-1) + ") + fib(" + (n-2) + ")");
+        } else {
+            System.out.println("return fib(" + (n - 1) + ") + fib(" + (n - 2) + ")");
             return fib(n - 1) + fib(n - 2);
         }
     }
 
-    public static int fib2(int n){
-        if (n == 0){
+    public static int fib2(int n) {
+        if (n == 0) {
             System.out.println("1");
             return 1;
-        }
-        else if (n == 1){
+        } else if (n == 1) {
             System.out.println("1 1");
             return 1;
-        }
-        else{
+        } else {
             System.out.print("1 1");
             int prev2 = 1;
             int prev1 = 1;
             int current = prev2 + prev1;
-            n-=3;
-            while (n >= 0){
+            n -= 3;
+            while (n >= 0) {
                 System.out.print(" " + current);
                 prev2 = prev1;
                 prev1 = current;
@@ -39,24 +36,11 @@ public class Program {
             System.out.print(" " + current);
             return current;
         }
-
     }
-
-//    public int showNFibonacci(int n) {
-//        for (int i = 1; i<=n; i++) {
-//            if (n <= 1) {
-//                return n;
-//            }
-//            return showNFibonacci(n - 1) + showNFibonacci(n - 2);
-//        }
-//
-//    }
 
     public static void main(String[] args) {
         Program.fib2(5);
     }
-
 }
 
-//
 ////Napisz program, który pobiera liczbê n i wyœwietla n pocz¹tkowych liczb ci¹gu Fibonacciego.
--- src/Alg8/Exe.java
+++ src/Alg8/Exe.java
@@ -26,28 +26,101 @@ public class Exe {
         System.out.println(divByA);
     }
 
-//    static void isPrimary(int a, int b) {
-//        int[] isPrimary = new int[b - a + 1];
-//        int index = 0;
-//
-//        for (int n = a; n <= b; n++) {
-//            if (n != n || n != 1) {
-//                for (int i = 0; i <= n; i++) {
-//                    if (n % 1 == 0) {
-//                        isPrimary[0] = n;
-//                        index++;
-//                    }
-//                }
-//            }
-//        }
-//    }
+    static void isPrimary(int a, int b) {
+        int[] isPrimary = new int[b - a + 1];
+        int index = 0;
+        boolean isPrime = false;
+
+        for (int n = a; n <= b; n++) {
+
+            for (int i = 2; i <= n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                } else {
+                    isPrime = true;
+                    isPrimary[index] = n;
+                    index++;
+                }
+            }
+        }
+        System.out.println(isPrimary);
+    }
+
+    static void isOdd(int a, int b) {
+        int[] odd = new int[b - a + 1];
+        int index = 0;
+
+        for (int n = a; n <= b; n++) {
+            if (n % 2 == 1) {
+                odd[index] = n;
+                index++;
+            }
+        }
+        System.out.println(odd);
+    }
+
+    static void fib(int a, int b) {
+        int[] fib = new int[b - a + 1];
+        int index = 0;
+
+        for (int n = a; n <= b; n++) {
+            if (n == 0)
+                System.out.println("1");
+
+            else if (n == 1)
+                System.out.println("1 1");
+
+            else {
+                int prev2 = 1;
+                int prev1 = 1;
+                int current = prev2 + prev1;
+                n -= 3;
+
+                while (n >= a && n <= b) {
+                    prev2 = prev1;
+                    prev1 = current;
+                    current = prev2 + prev1;
+                    fib[index] = current;
+                    index++;
+                    --n;
+                }
+            }
+        }
+    }
+
+    static void sumNumbers(int a, int b) {
+        int sum = 0;
+        for (int n = a; n <= b; n++) {
+            sum = +n;
+        }
+        System.out.println(sum);
+    }
+
+    static void sumDigits(int a, int b) {
+        int sum = 0;
+
+        for (int n = a; n <= b; n++) {
+            if (n > 10)
+                sum += n % 10;
+            else
+                sum += n;
+        }
+        System.out.println(sum);
+    }
 
     static void showNumbers(int a, int b) {
         if (a < b) {
             raiseToPower(a, b);
             divByA(a, b);
+            isPrimary(a, b);
+            isOdd(a, b);
+            fib(a, b);
+            sumNumbers(a,b);
+            sumDigits(a,b);
+
         }
     }
+
 
     public static void main(String[] args) {
         showNumbers(3, 6);
